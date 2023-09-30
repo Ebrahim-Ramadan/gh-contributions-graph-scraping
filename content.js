cc72e7d7e3662453ea06b982858d9ea0c7cf4978
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-30 22:43:21 +0200

    wtf

--- content.js
+++ content.js
@@ -1,23 +1,13 @@
-const displayTableData = () => {
-    const dataContainer = document.getElementById('ghcontributions');
-    dataContainer.textContent = 'kkkkk';
+function scrapeTableAndSendData() {
+  const table = document.querySelector('.ContributionCalendar-grid.js-calendar-graph-table');
+  console.log('table', table);
+
+  if (chrome && chrome.runtime) {
+    chrome.runtime.sendMessage({tableData: table.innerHTML}, function(response) {
+      console.log(response);
+    });
+  }
 }
 
-  
-// Function to scrape the table and send the data to the extension popup
-function scrapeTableAndSendData() {
-    const table = document.querySelector('.ContributionCalendar-grid.js-calendar-graph-table');
-    console.log('table', table);
-    if (table) {
-      const rows = Array.from(table.querySelectorAll('tr'));
-      const tableData = rows.map(row =>
-        Array.from(row.querySelectorAll('td')).map(cell => cell.textContent.trim())
-      );
-      // Send the table data to the extension popup
-      chrome.runtime.sendMessage({ table });
-    }
-  }
-  
-  // Run the scraping and data sending function when the page is loaded
-  window.addEventListener('load', scrapeTableAndSendData);
-  
+// Run the scraping and data sending function when the page is loaded
+window.addEventListener('load', scrapeTableAndSendData);
--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log('Received message:', message);
-    if (message.table) {
-        displayTableData(message.table);
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // Check if the request is for the table data
+    if (request.tableData) {
+      // Display the table data in the ghcontributions element
+      const ghContributions = document.getElementById('ghcontributions');
+      ghContributions.innerHTML = request.tableData;
     }
-});
+  });
